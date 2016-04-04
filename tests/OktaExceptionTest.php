1b50455c7b79250905bf4a25013ae4f28d71c78d
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-04-04 09:28:07 -0700

    Added OktaRequestTest case and cleaned up other tests

--- tests/OktaExceptionTest.php
+++ tests/OktaExceptionTest.php
@@ -1,27 +1,20 @@
 <?php
 
-use Okta\Client as OktaClient;
-use Okta\Exception as OktaException;
-
 class OktaExceptionTest extends PHPUnit_Framework_TestCase
 {
 
-    protected $okta;
     protected $exception;
 
-    public function __construct() {
-        $this->okta      = new OktaClient('test', 'not_a_real_key');
-        $this->exception = new OktaException(new stdClass);
+    public function setUp() {
+        $this->exception = new Okta\Exception(new stdClass);
     }
 
     public function testExceptionObject() {
         $this->assertInstanceOf('Okta\Exception', $this->exception);
     }
 
-    /**
-     * @expectedException Okta\Exception
-     */
     public function testThrownException() {
+        $this->setExpectedException('Okta\Exception');
         throw $this->exception;
     }
 
